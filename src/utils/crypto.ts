91aded2867b954f0e2e08a1332739630fa204bda
Alberto Frattini <frattini.alberto@gmail.com>
2022-08-10 13:34:53 +0200

    Improve crypto module

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,27 +1,38 @@
+import crypto from 'crypto'
 import * as bcrypt from 'bcrypt'
 import * as jwt from 'jsonwebtoken'
 import type { User } from '../@types/index'
+import config from '../config'
+
+const pepperify = (password: string) => {
+    return crypto.createHmac('sha1', config.auth.secret).update(password).digest('hex')
+}
 
 export const hashPassword = async (password: string) => {
-    const hash = await bcrypt.hash(password, 10)
+    const spicyPassword = pepperify(password)
+    const hash = await bcrypt.hash(spicyPassword, config.auth.saltRounds)
     return hash
 }
 
 export const comparePassword = async (password: string, hashedPassword: string) => {
-    const isCorrect = await bcrypt.compare(password, hashedPassword)
+    const spicyPassword = pepperify(password)
+    const isCorrect = await bcrypt.compare(spicyPassword, hashedPassword)
     return isCorrect
 }
 
 export const generateAccessToken = (user: User) => {
-    const token = jwt.sign(user, 'TODO:makethissecret', {expiresIn: '2h'})
+    const token = jwt.sign(user, config.auth.secret, config.auth.generateOptions)
     return token
 }
 
 export const compareAccessToken = (token: string) => {
     try {
-        const result = jwt.verify(token, 'TODO:makethissecret')
+        const result = jwt.verify(token, config.auth.secret, config.auth.verifyOptions)
         return result
     } catch (e) {
+        if (e instanceof jwt.JsonWebTokenError || e instanceof SyntaxError) {
+            throw new Error('JSONWebToken error')
+        }
         throw new Error('Token does not exist or expired')
     }
 }
